d0ea5d1f3f6743fff26ee1958f69cf008b149d56
sozumaczu <sozumaczu@gmail.com>
2020-11-23 23:36:43 +0900

    copied from jekyll

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Github.io 블로그 시작",
+        "excerpt":"Github.io 페이지를 만들어 보았다.   기술 관련 정리를 하거나, 코드를 올리기에 github.io가 적당해 보인다.  Markdown 형식에 얼른 익숙해져야겠다.   // 코드는 이런식으로 올리면 되겠다. int main() {     printf(\"hello, world!\\n\");     return 0; }   사이트 내 링크는 이런식으로.. 표시 link   ","categories": ["Post"],
+        "tags": [],
+        "url": "http://localhost:4000/post/first-post/",
+        "teaser": null
+      }]
